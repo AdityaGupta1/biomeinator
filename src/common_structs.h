3e894566f619dbcfa98404b9db09037441cd8370
AdityaGupta1 <adityag@gmail.com>
2025-05-26 22:19:20 -0700

    move shaders around, add vertex struct definition to be used later

---
+++ src/common_structs.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <WindowsNumerics.h>
+
+using namespace Windows::Foundation::Numerics;
+
+struct Vertex
+{
+	float3 pos;
+	float3 nor;
+	float2 uv;
+};
---
+++ src/shaders/common_structs.hlsl
@@ -0,0 +1,6 @@
+struct Vertex
+{
+    float3 pos : POSITION;
+    float3 nor : POSITION;
+    float2 uv : TEXCOORD0;
+};
