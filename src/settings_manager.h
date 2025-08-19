405e150286564b1b412ce225b51654dfa0e23b1e
Aditya Gupta <adityag@gmail.com>
2025-08-18 20:49:23 -0700

    test output command line argument (#105)

--- src/settings_manager.h
+++ src/settings_manager.h
@@ -7,6 +7,8 @@ namespace SettingsManager
 
 void parseArgs(const int argc, const char* const* argv);
 
+bool hasOption(const std::string& name);
+
 bool getAsBool(const std::string& name);
 int getAsInt(const std::string& name);
 uint32_t getAsUint(const std::string& name);
