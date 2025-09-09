98fb3381510e78a4ad1e9dc9cf5615030a445c17
AdityaGupta1 <adityag@gmail.com>
2025-09-08 21:25:46 -0700

    formatting

--- src/shaders/fullscreen.vs.hlsl
+++ src/shaders/fullscreen.vs.hlsl
@@ -27,7 +27,7 @@ VsOut vsMain(uint id : SV_VertexID)
     // (-1,-1), (3,-1), (-1,3) covers screen without diagonal seam
     float2 pos = (id == 0) ? float2(-1, -1) :
                  (id == 1) ? float2(3, -1) :
-                              float2(-1, 3);
+                             float2(-1, 3);
     VsOut vsOut;
     vsOut.pos = float4(pos, 0, 1);
     vsOut.uv = 0.5f * pos + 0.5f; // map clip-space to [0,1]
