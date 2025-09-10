c0bf330d3edb727aff091df9a9597425e1feefa0
AdityaGupta1 <adityag@gmail.com>
2025-09-10 10:30:10 -0700

    combo box and scoped item width

--- src/rendering/renderer.cpp
+++ src/rendering/renderer.cpp
@@ -866,6 +866,8 @@ void finalizeQueuedScreenshot()
     screenshotRequest = ScreenshotRequest();
 }
 
+static std::vector<const char*> tonemappingComboOptions = { "none", "standard", "AgX", "Khronos PBR neutral" };
+
 void imguiBeginFrame()
 {
     ImGui_ImplDX12_NewFrame();
@@ -877,13 +879,10 @@ void imguiBeginFrame()
 
     ImGui::Begin("Settings");
 
-    ImGui::PushItemWidth(120.f);
-
     SettingsGuiHelpers::InputUint("Samples per pixel", "spp", 1, 256);
     SettingsGuiHelpers::InputUint("Max path depth", "maxPathDepth", 1, 16);
     SettingsGuiHelpers::Checkbox("Enable MIS", "enableMis");
-
-    ImGui::PopItemWidth();
+    SettingsGuiHelpers::ComboUint("Tonemapping", "tonemapping", tonemappingComboOptions);
 
     ImGui::End();
 }
--- src/settings_gui_helpers.h
+++ src/settings_gui_helpers.h
@@ -23,9 +23,29 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include <imgui.h>
 
 #include <algorithm>
+#include <vector>
+
+#include "debug.h"
 
 namespace SettingsGuiHelpers
 {
+
+constexpr float inputWidth = 120.f;
+constexpr float sliderWidth = 200.f;
+constexpr float comboWidth = 200.f;
+
+struct ScopedItemWidth
+{
+    ScopedItemWidth(float width)
+    {
+        ImGui::PushItemWidth(width);
+    }
+
+    ~ScopedItemWidth()
+    {
+        ImGui::PopItemWidth();
+    }
+};
 
 inline void Checkbox(const char* label, const char* settingName)
 {
@@ -38,6 +58,7 @@ inline void Checkbox(const char* label, const char* settingName)
 
 inline void InputInt(const char* label, const char* settingName, int minVal, int maxVal, int step = 1)
 {
+    ScopedItemWidth width(inputWidth);
     int value = SettingsManager::getAsInt(settingName);
     if (ImGui::InputInt(label, &value, step))
     {
@@ -48,6 +69,7 @@ inline void InputInt(const char* label, const char* settingName, int minVal, int
 
 inline void SliderInt(const char* label, const char* settingName, int minVal, int maxVal)
 {
+    ScopedItemWidth width(sliderWidth);
     int value = SettingsManager::getAsInt(settingName);
     if (ImGui::SliderInt(label, &value, minVal, maxVal))
     {
@@ -58,6 +80,7 @@ inline void SliderInt(const char* label, const char* settingName, int minVal, in
 
 inline void InputUint(const char* label, const char* settingName, int minVal, int maxVal, int step = 1)
 {
+    ScopedItemWidth width(inputWidth);
     int value = static_cast<int>(SettingsManager::getAsUint(settingName));
     if (ImGui::InputInt(label, &value, step))
     {
@@ -68,10 +91,22 @@ inline void InputUint(const char* label, const char* settingName, int minVal, in
 
 inline void SliderUint(const char* label, const char* settingName, int minVal, int maxVal)
 {
+    ScopedItemWidth width(sliderWidth);
     int value = static_cast<int>(SettingsManager::getAsUint(settingName));
     if (ImGui::SliderInt(label, &value, minVal, maxVal))
     {
         value = std::clamp(std::max(value, 0), minVal, maxVal);
+        SettingsManager::setAsUint(settingName, static_cast<uint32_t>(value));
+    }
+}
+
+inline void ComboUint(const char* label, const char* settingName, const std::vector<const char*>& items)
+{
+    ScopedItemWidth width(comboWidth);
+    int value = static_cast<int>(SettingsManager::getAsUint(settingName));
+    ASSERT(value < items.size());
+    if (ImGui::Combo(label, &value, items.data(), static_cast<int>(items.size())))
+    {
         SettingsManager::setAsUint(settingName, static_cast<uint32_t>(value));
     }
 }
