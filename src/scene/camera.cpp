1ab09c65f7af9defd2a699cac736bbfa6c5cd640
Aditya Gupta <adityag@gmail.com>
2025-09-13 22:18:19 -0700

    Revert "idk"
    This reverts commit ffc8322025dab5b2d3932be23953bbf19b4dd23c.

--- src/scene/camera.cpp
+++ src/scene/camera.cpp
@@ -96,7 +96,6 @@ void Camera::setMatrices()
         this->currentFovYRadians, this->aspectRatio, this->params.nearPlane, this->params.farPlane);
 
     const XMMATRIX worldToClip = XMMatrixMultiply(worldToView, viewToClip);
-    this->params.worldToPrevClipMat = this->params.worldToClipMat;
     XMStoreFloat4x4(&this->params.worldToClipMat, worldToClip);
 
     XMStoreFloat4x4(&this->dlssMatrices.viewToClipMat, viewToClip);
@@ -165,6 +164,7 @@ void Camera::update(double deltaTime, const PlayerInput& input)
         this->areMatricesDirty = true;
     }
 
+    this->params.worldToPrevClipMat = this->params.worldToClipMat;
     if (this->areMatricesDirty)
     {
         this->setMatrices();
