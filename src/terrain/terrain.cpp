b6d6a795b58f918f7b5238afd291ad7d59383067
AdityaGupta1 <adityag@gmail.com>
2026-01-19 18:28:03 -0800

    increase max tasks per frame

--- src/terrain/terrain.cpp
+++ src/terrain/terrain.cpp
@@ -37,8 +37,6 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #define CREATE_BLAS_DISTANCE (RENDER_DISTANCE + 1)
 #define CREATE_BLOCKS_DISTANCE (CREATE_BLAS_DISTANCE + 1)
 
-#define MAX_TASKS_PER_FRAME 10 // TODO: revisit this once BLAS builds are spread out
-
 namespace Terrain
 {
 
@@ -238,12 +236,14 @@ void update(ToFreeList& toFreeList)
         tasksToEnqueue.push_back({ task_createInstance, chunk });
     }
 
+    static constexpr uint32_t maxTasksPerFrame = 32;
+
     if (!tasksToEnqueue.empty())
     {
         std::vector<Task> thisFrameTasks;
-        thisFrameTasks.reserve(MAX_TASKS_PER_FRAME);
+        thisFrameTasks.reserve(maxTasksPerFrame);
 
-        for (uint32_t i = 0; i < MAX_TASKS_PER_FRAME && !tasksToEnqueue.empty(); ++i)
+        for (uint32_t i = 0; i < maxTasksPerFrame && !tasksToEnqueue.empty(); ++i)
         {
             thisFrameTasks.push_back(tasksToEnqueue.front());
             tasksToEnqueue.pop_front();
