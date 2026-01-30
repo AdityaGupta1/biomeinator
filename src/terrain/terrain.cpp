5e536577acd5026e8303007691afa47b852d7aa6
AdityaGupta1 <adityag@gmail.com>
2026-01-30 00:08:49 -0800

    chunk generation at ludicrous speed

--- src/terrain/terrain.cpp
+++ src/terrain/terrain.cpp
@@ -117,7 +117,7 @@ void setDirty()
 static glm::ivec2 lastChunkPos{ INT_MAX, INT_MAX };
 
 inline constexpr uint32_t maxTasksPerFrame = 48;
-inline constexpr uint32_t maxNumGenerateBlocksTasksPerFrame = 4;
+inline constexpr uint32_t maxNumGenerateBlocksTasksPerFrame = 6;
 
 void update(ToFreeList& toFreeList)
 {
