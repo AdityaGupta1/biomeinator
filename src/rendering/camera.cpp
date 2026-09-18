17724bb3239d4d2c2d28850e7f0ae6eefdf7340a
AdityaGupta1 <adityag@gmail.com>
2026-09-17 18:58:00 -0700

    Extend camera far plane to 100000 blocks

--- src/rendering/camera.cpp
+++ src/rendering/camera.cpp
@@ -29,7 +29,7 @@ void Camera::init(float defaultFovYRadians)
     this->setDirectionVectorsFromAngles();
 
     this->params.nearPlane = 0.1f;
-    this->params.farPlane = 10000.f;
+    this->params.farPlane = 100000.f;
 
     XMMATRIX identity = XMMatrixIdentity();
     XMStoreFloat4x4(&this->worldToPrevViewMat, identity);
